4b69708e190c9f300925e1479355026232434e52
vivek <vivek_msn@hotmail.com>
2024-12-10 23:37:48 +0530

    using add_menu_page function and hook in to admin menu

--- wp-custom-woocom-product.php
+++ wp-custom-woocom-product.php
@@ -11,3 +11,17 @@
  if(!defined("ABSPATH")){
     exit;
  }
+
+ //Add Plugin Menu
+ add_action( "admin_menu", "wcp_add_menu" );
+
+ function wcp_add_menu(){
+
+    add_menu_page( "WooCommerce Product Creator", "WooCommerce Product Creator", "manage_options",
+     "wcp-woocommerce-product-creator", "wcp_add_woocommerce_product_layout", "dashicons-cloud-upload", 8);
+ }
+
+ //Add WooCommerce Product Layout
+ function wcp_add_woocommerce_product_layout(){
+    echo "<h3>Add WooCommerce Product</h3>";
+ }
