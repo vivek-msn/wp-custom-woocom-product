ece1660d469c3f0ce84974cdb9b0dfb065a4ea9f
vivek <vivek_msn@hotmail.com>
2024-12-10 23:23:28 +0530

    create folder structure and header requirement of plugin

---
+++ wp-custom-woocom-product.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Custom WooCommerce Product Creator
+ * Description: This plugin create woocommerce product
+ * Version: 1.0
+ * Author: Vivek Saini
+ * Plugin URI: https//testing.com/custom-woocommerce-product
+ */
+
+ if(!defined("ABSPATH")){
+    exit;
+ }
